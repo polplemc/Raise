28ecfba9322808f73c7285a7ab5c6bd6132cd284
polplemc <comaapple90@gmail.com>
2025-11-18 01:05:58 +0800

    create admin - modify

--- createsuperuser.py
+++ createsuperuser.py
@@ -1,11 +1,18 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")  # <-- change myproject
+
+django.setup()
+
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 
 User = get_user_model()
 
 username = "admin"
-email = "admin@raise.com"
-password = "12345"   # change to your password
+email = "admin@example.com"
+password = "admin123"
 
 try:
     User.objects.create_superuser(username=username, email=email, password=password)
