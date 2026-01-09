b72e052e32e5c9f026141eb7494362bc434867c0
polplemc <comaapple90@gmail.com>
2026-01-09 18:58:30 +0800

    update102

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -166,16 +166,3 @@ LOGGING = {
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if os.environ.get("DJANGO_SUPERUSER_USERNAME"):
-    username = os.environ["DJANGO_SUPERUSER_USERNAME"]
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=os.environ.get("DJANGO_SUPERUSER_EMAIL", ""),
-            password=os.environ["DJANGO_SUPERUSER_PASSWORD"],
-        )
