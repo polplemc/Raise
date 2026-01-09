39e01b3974534cafa1f2a507befdd82c3518d83d
polplemc <comaapple90@gmail.com>
2026-01-09 21:29:49 +0800

    update108

--- myproject/myapp/core/apps.py
+++ myproject/myapp/core/apps.py
@@ -1,4 +1,3 @@
-# core/apps.py
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 from django.contrib.auth import get_user_model
@@ -16,7 +15,6 @@ class CoreConfig(AppConfig):
             password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
             if username and password and not User.objects.filter(username=username).exists():
-                print("Creating superuser...")  # debug
                 User.objects.create_superuser(username=username, email=email, password=password)
 
         post_migrate.connect(create_superuser, sender=self)
